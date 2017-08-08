777fd2d9be38f854d878cfd4ad3d3643d472ccc7
Akshay Rahul <akshayrahul97@gmail.com>
2017-08-08 22:27:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile("Article one yet to be updated");
+  res.send("Article one yet to be updated");
 });
 app.get('/article-two', function (req, res) {
-  res.sendFile("Article two yet to be updated");
+  res.send("Article two yet to be updated");
 });
 app.get('/article-three', function (req, res) {
-  res.sendFile("Article three yet to be updated");
+  res.send("Article three yet to be updated");
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
