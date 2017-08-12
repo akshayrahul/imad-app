fbb13e192b5e42499f58054d45197c781edc87c5
Akshay Rahul <akshayrahul97@gmail.com>
2017-08-12 19:24:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ var articleThree = {
 
 function createTemplate(data){
     var title = data.title;
+    var heading=data.heading;
     var content = data.content;
     var htmlTemplate = `
     <html>
@@ -99,13 +100,14 @@ h2{
 </html>
 
     `;
+    return htmlTemplate;
 }
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
