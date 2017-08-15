94ece1ae344e021e049d754fe7e609ea5139d1cd
Akshay Rahul <akshayrahul97@gmail.com>
2017-08-15 13:19:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 var center_text = document.getElementById("main-text");
 center_text.innerHTML ="Better Webapp";
 var img=document.getElementById("ping");
-img.oncLick = function(){
+img.onclick = function(){
     img.style.marginLeft = '100px';
 };
