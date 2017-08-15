9711148e73b55b48f9d2dc1dec700244e2e6996c
Akshay Rahul <akshayrahul97@gmail.com>
2017-08-15 16:23:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,18 @@
-console.log('Loaded!');
-var center_text = document.getElementById("main-text");
-center_text.innerHTML ="Better Webapp";
-var img=document.getElementById("ping");
-var marginLeft=0;
-function moveLeft(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';    
-}
-img.onclick = function(){
-    var interval = setInterval(moveLeft,5);
-};
+var button = document.getElementById("counter");
+var counter=0;
+Button.onclick = function(){
+    
+    var request= new XMLHTTPRequest();
+    request.onreadystatechange = function(){
+      if(request.readyState==XMLHTTPRequest.DONE){
+          if(request.status==200){
+              var counter= request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML=counter.toString();
+          }
+      }  
+    };
+    request.open("GET", "http://akshayrahul97.imad.hasura-app.io/counter",true);
+    request.send(null);
+
+}; 
