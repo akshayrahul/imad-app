c84d1870451e18b787163074d854cc6990ccce9e
Akshay Rahul <akshayrahul97@gmail.com>
2017-08-15 23:26:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,9 @@ button.onclick = function(){
     request.onreadystatechange = function(){
       if(request.readyState==XMLHttpRequest.DONE){
           if(request.status==200){
-              counter=counter+1;
+              var counter=request.responseText;
+              var span=document.getElementById("count");
+              span.innerHTML=counter.toString();
           }
       }  
     };
