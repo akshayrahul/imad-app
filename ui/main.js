918b73dd52ca27fe67cbdc0821d552b94dcd8d3d
Akshay Rahul <akshayrahul97@gmail.com>
2017-08-15 23:18:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,9 +6,7 @@ button.onclick = function(){
     request.onreadystatechange = function(){
       if(request.readyState==XMLHttpRequest.DONE){
           if(request.status==200){
-              var counter= request.responseText;
-                var span = document.getElementById("count");
-                span.innerHTML=counter.toString();
+              
           }
       }  
     };
@@ -17,12 +15,15 @@ button.onclick = function(){
 
 }; 
 
-var nameInput= document.getElementById("name");
-var name=nameInput.value;
 var submit=document.getElementById("btn");
 submit.onclick = function(){
-    
-    var names=["name1","name2","name3","name4"];
+      var request= new XMLHttpRequest();
+    request.onreadystatechange = function(){
+      if(request.readyState==XMLHttpRequest.DONE){
+          if(request.status==200){
+                
+    var names=request.responseText;
+    names=JSON.parse(names);
     var list=" ";
     for (var i=0;i<names.length;i++)
     {
@@ -30,4 +31,14 @@ submit.onclick = function(){
     }
     var ul=document.getElementById("namelist");
     ul.innerHTML = list;
+      
+          }
+      }  
+    };
+    var nameInput= document.getElementById("name");
+    var name=nameInput.value;
+
+    request.open("GET", "http://akshayrahul97.imad.hasura-app.io/counter",true);
+    request.send(null);
 };
+    
